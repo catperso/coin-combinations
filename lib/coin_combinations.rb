7a727ff789c3ea0ca9c830dfcb291e2e6ab7fd91
Matt Casperson <mc.casperson@gmail.com>
2021-12-09 10:50:08 -0800

    refactor coin_combinations.rb to pass both specs more concisely
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -7,14 +7,54 @@ class CoinCombiner
   end
 
   def counter(cash)
-    @pennies = cash.to_i
-    penny_word = 'pennies'
-    if @pennies == 1
-      penny_word = 'penny'
+    output = []
+    remainder_cash = cash.to_i
+    remainder_cash = nickels(remainder_cash)
+    remainder_cash = pennies(remainder_cash)
+
+    if @nickels > 0
+      if output.length > 0
+        output.push("and")
+      end
+      output.push("#{@nickels}")
+      if @nickels == 1
+        output.push("nickel")
+      else
+        output.push("nickels")
+      end
     end
     if @pennies > 0
-      "#{@pennies} pennies"
+      if output.length > 0
+        output.push("and")
+      end
+      output.push("#{@pennies}")
+      if @pennies == 1
+        output.push("penny")
+      else
+        output.push("pennies")
+      end
     end
+    output.join(' ')
   end
 
+  private
+    def quarters(cash)
+      @quarters = (cash - (cash % 25)) / 25
+      cash % 25
+    end
+
+    def dimes(remainder)
+      @dimes = (remainder - (remainder % 10)) / 10
+      remainder % 10
+    end
+
+    def nickels(remainder)
+      @nickels = (remainder - (remainder % 5)) / 5
+      remainder % 5
+    end
+
+    def pennies(remainder)
+      @pennies = remainder
+      0
+    end
 end
